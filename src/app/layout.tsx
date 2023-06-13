937a499b4b3a19c412c64a3a598fbac277ec1b69
isZachariah <103608503+isZachariah@users.noreply.github.com>
2023-06-13 10:22:28 -0700

    added some styling to the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,24 +17,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className={'h-100 bg-slate-500 flex flex-row w-screen align-middle'}>
-          <h1 className={'text-lg align-middle p-0 pl-4 pt-3 m-0 w-screen'}>
-            <Link href={'/'}>Zachariah Angus Magee</Link>
-          </h1>
-          <nav>
-            <ul  className='flex flex-row justify-end align-middle m-0 p-0 gap-x-12 pt-3 pr-8'>
-              <li>
-                <Link href={'/code'}>code</Link>
-              </li>
-              <li>
-                <Link href={'/blog'}>blog</Link>
-              </li>
-              <li>
-                <Link href={'/etc'}>etc</Link>
-              </li>
-            </ul>
-          </nav>  
-        
+        <header className={'h-150 bg-slate-500 flex flex-row w-screen align-middle'}>
+          <div className={'h-150 flex flex-row w-screen align-middle'}>
+            <h1 className={'h-150 text-lg align-middle p-0 pl-4 pt-2 m-0 w-screen'}>
+              <Link href={'/'} className={'hover:underline'}>Zachariah Angus Magee</Link>
+            </h1>
+            <nav>
+              <ul  className='flex flex-row list-none justify-end align-middle m-0 p-0 gap-x-12 pt-3 pr-8'>
+                <li>
+                  <Link href={'/code'} className={'mr-4 hover:underline md:mr-6'}>code</Link>
+                </li>
+                <li>
+                  <Link href={'/blog'}  className={'mr-4 hover:underline md:mr-6'}>blog</Link>
+                </li>
+                <li>
+                  <Link href={'/etc'}  className={'mr-4 hover:underline md:mr-6'}>etc</Link>
+                </li>
+              </ul>
+            </nav>  
+          </div>
         </header>
         {children}
 
