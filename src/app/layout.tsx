8d0c60f44eb217db2a3eee7cb0a8b425959f884f
isZachariah <103608503+isZachariah@users.noreply.github.com>
2023-06-13 13:59:09 -0700

    changed some styles in the nav bar (yet again)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,28 +17,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className={'h-150 bg-slate-500 flex flex-row w-screen align-middle'}>
-          <div className={'h-150 flex flex-row w-screen align-middle'}>
+        <header className={'h-150 bg-slate-500 flex flex-row w-screen align-middle p-5'}>
+          <div className={'h-150 flex flex-row w-screen align-middle text-stone-200'}>
             <h1 className={'h-150 text-lg align-middle p-0 pl-4 pt-2 m-0 w-screen'}>
-              <Link href={'/'} className={'hover:underline'}>Zachariah Angus Magee</Link>
+              <Link href={'/'} className={'hover:bg-slate-600 hover:rounded p-3'}>Zachariah Angus Magee</Link>
             </h1>
             <nav>
               <ul  className='flex flex-row list-none justify-end align-middle m-0 p-0 gap-x-12 pt-3 pr-8'>
                 <li>
-                  <Link href={'/code'} className={'mr-4 hover:underline md:mr-6'}>code</Link>
+                  <Link href={'/code'} className={'mr-4 hover:bg-slate-600 hover:rounded p-3 md:mr-6'}>code</Link>
                 </li>
                 <li>
-                  <Link href={'/blog'}  className={'mr-4 hover:underline md:mr-6'}>blog</Link>
+                  <Link href={'/blog'}  className={'mr-4 hover:bg-slate-600 hover:rounded p-3 md:mr-6'}>blog</Link>
                 </li>
                 <li>
-                  <Link href={'/etc'}  className={'mr-4 hover:underline md:mr-6'}>etc</Link>
+                  <Link href={'/etc'}  className={'mr-4 hover:bg-slate-600 hover:rounded p-3 md:mr-6'}>etc</Link>
                 </li>
               </ul>
             </nav>  
           </div>
         </header>
         {children}
-
       </body>
     </html>
   )
